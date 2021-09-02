b095d3ca124064e1a3dcd4c376031b3b73a9d403
András Ács <5522871+andracs@users.noreply.github.com>
2021-09-02 11:31:38 +0200

    Rettet stavefejl

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello world!");
-        System.out.println("Hello datmatikere! 😀😀");
+        System.out.println("Hello datamatikere! 😀😀");
     }
 }
